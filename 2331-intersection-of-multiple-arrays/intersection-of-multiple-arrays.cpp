1dddf20be0c2b2b193629a5e782eebcc131b96c6
Deepu-shrivas <deepanshshrivas781@gmail.com>
2026-08-27 00:27:27 +0530

    Time: 1 ms (64.35%) | Memory: 15.7 MB (98.71%) - LeetSync

---
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersection(vector<vector<int>>& nums) {
+        vector<int> ans;
+
+        for (int x = 1; x <= 1000; x++) {
+            bool present = true;
+
+            for (auto &arr : nums) {
+                if (find(arr.begin(), arr.end(), x) == arr.end()) {
+                    present = false;
+                    break;
+                }
+            }
+
+            if (present)
+                ans.push_back(x);
+        }
+
+        return ans;
+    }
+};
